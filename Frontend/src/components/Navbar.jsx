d48b9e03a0a9ff67ab905458b1701ee1f8636d3b
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-07-01 12:56:49 +0100

    Added google font

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
 
               {/* sun icon */}
               <svg
-                className="swap-off h-8 w-8 fill-current"
+                className="swap-off h-7 w-7 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
               >
@@ -97,7 +97,7 @@ const Navbar = () => {
 
               {/* moon icon */}
               <svg
-                className="swap-on h-8 w-8 fill-current"
+                className="swap-on h-7 w-7 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
               >
